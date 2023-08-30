424bad3aee4767ed33a1d1b68ef9accecbba2ca0
Raphael De Lio <raphadlts@gmail.com>
2023-08-30 11:03:28 +0200

    Building my first page: Using the PlanetCard component in the main component

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,24 @@ export default defineComponent({
 });
 </script>
 
-<template>
-
-</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import PlanetCard from './components/PlanetCard.vue'; // Add this line
+import type { Planet } from './types';
+ 
+export default defineComponent({
+  components: { ./components/PlanetCard }, // Add this line
+  setup() {
+    const planets = ref<Planet[]>([
+      { name: 'Earth', discoveryYear: 0, lightYearsAway: 0 },
+      { name: 'Mars', discoveryYear: 1610, lightYearsAway: 0.000014 },
+      { name: 'CoRoT-8b', discoveryYear: 2010, lightYearsAway: 1050 },
+      // Add more planets here
+    ]);
+    return { planets };
+  },
+});
+</script>
 
 <style scoped>
 

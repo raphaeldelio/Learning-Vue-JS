aec708ff54379a075cd5aa145f2448948cb05a8b
Raphael De Lio <raphadlts@gmail.com>
2023-08-30 11:04:34 +0200

    Building my first page: Use the Card Component in the Main Component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
-import type { Planet } from './types'; // Adjust the path to match the location of your types file
-
-export default defineComponent({
-  setup() {
-    const planets = ref<Planet[]>([
-      { name: 'Earth', discoveryYear: 0, lightYearsAway: 0 },
-      { name: 'Mars', discoveryYear: 1610, lightYearsAway: 0.000014 },
-      { name: 'CoRoT-8b', discoveryYear: 2010, lightYearsAway: 1050 },
-      // Add more planets here
-    ]);
-    return { planets };
-  },
-});
-</script>
+<template>
+  <div>
+    <PlanetCard v-for="planet in planets" :key="planet.name" :planet="planet" />
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
@@ -21,7 +10,7 @@ import PlanetCard from './components/PlanetCard.vue'; // Add this line
 import type { Planet } from './types';
  
 export default defineComponent({
-  components: { ./components/PlanetCard }, // Add this line
+  components: { PlanetCard }, // Add this line
   setup() {
     const planets = ref<Planet[]>([
       { name: 'Earth', discoveryYear: 0, lightYearsAway: 0 },

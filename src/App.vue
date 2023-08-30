11f8afdf3d227d4dff0ec80657e5fb039e89cca9
Raphael De Lio <raphadlts@gmail.com>
2023-08-30 11:02:59 +0200

    Creating my first page: Defining the mock data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import type { Planet } from './types'; // Adjust the path to match the location of your types file
 
+export default defineComponent({
+  setup() {
+    const planets = ref<Planet[]>([
+      { name: 'Earth', discoveryYear: 0, lightYearsAway: 0 },
+      { name: 'Mars', discoveryYear: 1610, lightYearsAway: 0.000014 },
+      { name: 'CoRoT-8b', discoveryYear: 2010, lightYearsAway: 1050 },
+      // Add more planets here
+    ]);
+    return { planets };
+  },
+});
 </script>
 
 <template>

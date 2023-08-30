5eda84605552960d67e916bfc396f4c44dc10f13
Raphael De Lio <raphadlts@gmail.com>
2023-08-30 11:02:26 +0200

    Building my first app: Creating PlanetCard.vue component

---
+++ src/components/PlanetCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+      <h2>{{ planet.name }}</h2>
+      <p>Year of Discovery: {{ planet.discoveryYear }}</p>
+      <p>Light Years Away: {{ planet.lightYearsAway }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { Planet } from '../types'; 
+
+export default defineComponent({
+  props: {
+    planet: {
+      type: Object as () => Planet,
+      required: true,
+    },
+  },
+});
+</script>
